405b89b76c65a7cf691f3f8f291337dbb164dfa7
miss_k <86659575+daredevil3435@users.noreply.github.com>
2024-06-22 01:45:20 +0530

    added lc folder

---
+++ Leetcode/LC2373.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int n = grid.size();
+    vector<vector<int>> maxLocal(n - 2, vector<int>(n - 2, 0));
+
+    // Iterate over the grid to find max in each 3x3 submatrix
+    for (int i = 1; i < n - 1; ++i) {
+        for (int j = 1; j < n - 1; ++j) {
+            int maxVal = grid[i - 1][j - 1];
+            for (int k = i - 1; k <= i + 1; ++k) {
+                for (int l = j - 1; l <= j + 1; ++l) {
+                    maxVal = max(maxVal, grid[k][l]);
+                }
+            }
+            maxLocal[i - 1][j - 1] = maxVal;
+        }
+    }
+
+    return maxLocal;
+    }
+};
